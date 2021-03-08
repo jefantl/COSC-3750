9aaac8690803c8eaaf2781ce1cbd62211637d739
JasonFantl <jasonemerald@gmail.com>
2021-03-08 12:30:58 -0700

    HW5

--- HW4/wycat.c
+++ HW4/wycat.c
@@ -5,55 +5,46 @@
 *
 * COSC 3750, Homework 4
 *
-* This is the a simple version of the cat utility
+* This is a simple version of the cat utility.
 *
 */
 
 #include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
 #include <string.h>
 
-#define BUFFER_SIZE 16
+#define BUFFER_SIZE 1024
 
 void printBuffer(char buffer[], int size, char* filepath) {
-  int writeFlag = fwrite(buffer, size, 1, stdout);
-  if (writeFlag != 1) {
+  int writeFlag = fwrite(buffer, 1, size, stdout);
+  if (writeFlag != size) {
     perror(filepath);
   }
 }
 
 void printFile(FILE *in, char* filepath) {
   char buffer[BUFFER_SIZE];
-  int readFlag = 0;
+  int bytesRead = 0;
   do {
-    memset(buffer, 0, BUFFER_SIZE);
-
-    readFlag = fread(buffer, BUFFER_SIZE, 1, in);
+    bytesRead = fread(buffer, 1, BUFFER_SIZE, in);
     // check if the read diddn't fill the buffer
-    if (readFlag != 1) {
-      // check if we reached end of file
-      int eofFlag = feof(in);
-      clearerr(in);
-      if (eofFlag != 0) {
-        // find where eof was and print up to that
-        for (int i = 0; i < BUFFER_SIZE; i++) {
-          if (buffer[i] == 0) {
-            printBuffer(buffer, i, filepath);
-            break;
-          }
-        }
-      }
-      else {
+    if (bytesRead != BUFFER_SIZE) {
+      // check if we found an error
+      int errorFlag = ferror(in);
+      if (errorFlag != 0) {
         // this would be a read error
         perror(filepath);
+        clearerr(in);
         break;
       }
+      else {
+        printBuffer(buffer, bytesRead, filepath);
+      }
+      clearerr(in);
     }
     else {
       printBuffer(buffer, BUFFER_SIZE, filepath);
     }
-  } while (readFlag == 1);
+  } while (bytesRead == BUFFER_SIZE);
 }
 
 void printUserInput() {
